456ac6746fc9bcede076ca48e26d2eca2ca0c070
git cole.li <git@cole.li>
2021-02-24 22:41:39 +0000

    simple 1

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+"""
+high level support for doing this and that.
+"""
 from flask import Flask
 
 
@@ -6,8 +9,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Congratulations, it's a web app!"
-    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
     
-
