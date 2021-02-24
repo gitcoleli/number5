40b46818060ce6ccbdb727e292c983fe832f2a66
git cole.li <git@cole.li>
2021-02-24 22:19:40 +0000

    dd

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-#Import the flask module
-from flask import import Flask
+from flask import Flask
 
-#Create a Flask constructor. It takes name of the current module as the argument
 app = Flask(__name__)
 
-#Create a route decorator to tell the application, which URL should be called for the #described function and define the function
-
-@app.route('/')
-def tutorialspoint():
-return “Welcome to TutorialsPoint”
-
-#Create the main driver function
-if __name__ == ‘__main__’:
-#call the run method
-app.run()
+@app.route("/")
+def index():
+    return "Congratulations, it's a web app!"
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080, debug=True)
