5fe441beecc369c436186d166aa827e0159c98f4
git cole.li <git@cole.li>
2021-02-24 22:34:31 +0000

    testing linting

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from sys import exit
+
 
 app = Flask(__name__)
 
@@ -10,6 +10,4 @@ def index():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
     
-# Code!
 
-exit(0) # Successful exit
