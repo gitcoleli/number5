950a5ee603bc162a81fddb2bd95e8610d5c9d606
gitcoleli <git@cole.li>
2021-03-07 19:57:22 +0000

    update 1

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def hello_blue():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port='5000')
+    app.run(debug=True, host='0.0.0.0', port='80')
