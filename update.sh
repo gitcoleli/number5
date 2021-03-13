a6d97273c2a468b8c5e19efd9c9461c69faf1bc5
gitcoleli <git@cole.li>
2021-03-13 14:50:15 +0000

    fadfa

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 SEARCH="xxx"
-REPLACE="${CIRCLE_WORKFLOW_ID:0:8}"
 
-cat k8s/deployment.yml | sed -e "s/$SEARCH/$REPLACE/"
+REPLACE="${CIRCLE_BUILD_NUM}"
+
+cat deployment.yml | sed -e "s/$SEARCH/$REPLACE/" >> deployment2.yml
