65b3dc52bf2b643c5d0a46066b1d642a1fc99fa8
gitcoleli <git@cole.li>
2021-03-13 21:21:04 +0000

    deploy green

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_blue():
+def udcity():
     return render_template('index.html')
 
 
--- update.sh
+++ update.sh
@@ -3,3 +3,5 @@ SEARCH="xxx"
 REPLACE="${CIRCLE_WORKFLOW_ID:0:8}"
 
 cat k8s/deployment.yml | sed -e "s/$SEARCH/$REPLACE/" >> deployment2.yml
+
+# used to replace the tag and allow us to deploy new image
